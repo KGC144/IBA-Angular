cb5824dde28db9e662a1ea90e28ed465183f51c0
kgch <kgch4428@gmail.com>
2021-06-07 12:31:47 +0530

    Initial commit

---
+++ src/app/admin/admin-actions/admin-actions.component.ts
@@ -0,0 +1,69 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { IAccount } from 'src/app/Account';
+import { ICustomer } from 'src/app/Customer';
+import { CustomerService } from 'src/app/customer.service';
+import { ITransaction } from 'src/app/Transaction';
+
+
+@Component({
+  selector: 'app-admin-actions',
+  templateUrl: './admin-actions.component.html',
+  styleUrls: ['./admin-actions.component.css']
+})
+export class AdminActionsComponent implements OnInit {
+
+  customerId!: number;
+  customer!: ICustomer;
+  account!: IAccount;
+  customerAccountId!: number;
+  transactions!: ITransaction[];
+
+  constructor(private router: Router, private route: ActivatedRoute, private _customerService: CustomerService) { }
+
+  ngOnInit(): void {
+
+    const id = Number(this.route.snapshot.paramMap.get("id"));
+    console.log(id);
+    this.customerId = id;
+
+    this._customerService.getCustomer(id).subscribe({
+      next: data => this.customer = data,
+      error: err => console.log('error', err)
+    });
+
+    this._customerService.getAccountByCustomerId(id).subscribe({
+      next: data => this.account = data,
+      error: err => console.log('error', err)
+    });   
+
+  }
+ 
+
+
+  getTransactionsByAccountId(){
+
+    // this._customerService.getAllTransactionsByAccountId(this.account.accountId).subscribe({
+    //   next: data => this.transactions = data,
+    //   error: err => console.log('error', err)
+    // });admin-custran
+    this.router.navigate(["/admin-custran",this.account.accountId]);
+
+  }
+
+  onAccountDetails(){
+
+    this.router.navigate(["/account", this.account.accountId]);
+  }
+ 
+
+  listAllCustomers(){
+    this._customerService.listAllCustomers().subscribe(
+      data => console.log('Success!', data),
+      error => console.log('Error!', error)
+    );
+}
+
+
+
+}
